4dc982e26aa047b386b422250216bcc54c951c4c
Thamme Gowda N. <tgowdan@gmail.com>
2016-02-27 11:51:09 -0800

    Update script

--- namostats/scripts/savetweets.groovy
+++ namostats/scripts/savetweets.groovy
@@ -1,3 +1,41 @@
+import twitter4j.Query
+import twitter4j.QueryResult
+import twitter4j.TwitterObjectFactory
+
 tw = ctx.twitterService
-tw.saveAllTweets('thammegowda', 'thammegowda.txt')
-println("Done")
+tc = tw.twitter
+Query q = new Query("#Decision2016")
+q.setCount(100)
+q.setSinceId(0)
+q.setResultType(Query.ResultType.recent)
+q.setMaxId(700225107239833600)
+int count = 0
+new File("decision2016-2.txt").withWriter {out ->
+    while (true) {
+
+        QueryResult result = tc.search(q)
+        def statuses = result.getTweets()
+        if (statuses == null || statuses.empty) {
+            println("End")
+            break
+        }
+        result.rateLimitStatus
+        for (def s in statuses) {
+            out.println(TwitterObjectFactory.getRawJSON(s))
+            //print(s.user.id + "::" + s.createdAt + " :: " + s.text)
+            count++
+        }
+        println("Count = $count")
+        def rLimit = result.rateLimitStatus
+        println("Remaining ${rLimit.remaining}, resets in ${rLimit.resetTimeInSeconds} Reset:${rLimit.secondsUntilReset}" )
+        if(rLimit.remaining < 1){
+            print("going to sleep for ${rLimit.secondsUntilReset}")
+            Thread.sleep(1000 * (rLimit.secondsUntilReset + 1))
+        }
+        q.setMaxId(statuses.get(statuses.size() -1).id)
+        q.setCount(100)
+        q.setSinceId(0)
+        q.setResultType(Query.ResultType.recent)
+        Thread.sleep(2000)
+    }
+}
